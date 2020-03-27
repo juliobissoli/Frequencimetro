880d56c84afd372524f3e0ba0e7bd12bdb2ea20f
Julio Bissoli <juliobissoli33@gmail.com>
2020-03-27 10:08:17 -0300

    Add file login

--- src/router/index.js
+++ src/router/index.js
@@ -18,3 +18,45 @@ const router = new VueRouter({
 })
 
 export default router
+
+
+
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import Auth from '../utils/auth'
+// import Login from '../views/Login'
+// import FrequencimetroRouter from './frequencimetro'
+
+
+// Vue.use(VueRouter)
+
+// const routes = [
+//   {
+//     path: '/login',
+//     name: 'Login',
+//     component: Login
+//   }
+// ]
+// routes.push(FrequencimetroRouter)
+
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   linkExactActiveClass: 'active',
+//   routes
+// })
+
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some(record => record.meta.requiresAuth) && !Auth.loggedIn()) {
+//     next({
+//       path: '/login',
+//       query: { redirect: to.fullPath }
+//     })
+//   } else if (to.matched.some(record => record.name === 'Login') && Auth.loggedIn()) {
+//     next({ name: 'Campaign' })
+//   } else {
+//     next()
+//   }
+// })
+
+// export default router
